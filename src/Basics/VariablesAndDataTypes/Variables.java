3f566604337c0188ad359a21f992bb17826dab2c
Koduru Tarushank <kodurutarushank@Kodurus-MacBook-Air-2.local>
2021-02-10 10:24:02 -0500

    Updating new programs

---
+++ src/Basics/VariablesAndDataTypes/Variables.java
@@ -0,0 +1,22 @@
+/*
+ * @Author: Tarushank
+ * @Purpose: Showing how to declare variables.
+ */
+package Basics.VariablesAndDataTypes;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        //Declaring variables.
+        //Variable is a placeholder in memory associated with information.
+
+        //To hold an integer.
+        //Printing the integer.
+        int value = 10;
+        System.out.println("The value of the integer variable is: " +value);
+
+        //To hold a String.
+        String text = "This is a string";
+        System.out.println("This is a String: " +text);
+    }
+}
