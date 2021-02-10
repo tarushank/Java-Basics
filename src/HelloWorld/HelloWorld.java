ec34fb79e3eae945e843092c3a8c2f43925fb3b2
Koduru Tarushank <kodurutarushank@Kodurus-MacBook-Air-2.local>
2021-02-10 09:57:57 -0500

    First-program

---
+++ src/HelloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * @Author: Tarushank
+ * @Purpose: Every program starts with a hello-world.
+ */
+
+package HelloWorld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        //Printing a statement.
+        //Output: Hello-World.
+        System.out.println("Hello-World");
+    }
+}
